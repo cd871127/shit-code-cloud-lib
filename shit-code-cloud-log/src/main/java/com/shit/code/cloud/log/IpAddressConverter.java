3eaceb53196019428e2060e9b4f06bb80c75e39d
anthonychen <anthonychen@lexin.com>
2020-08-13 14:31:57 +0800

    fix

--- shit-code-cloud-log/src/main/java/com/shit/code/cloud/log/IpAddressConverter.java
+++ shit-code-cloud-log/src/main/java/com/shit/code/cloud/log/IpAddressConverter.java
@@ -3,18 +3,27 @@ package com.shit.code.cloud.log;
 
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import com.shit.code.cloud.common.NetUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author Anthony Chen
  * @date 2020/8/13
  **/
 public class IpAddressConverter extends ClassicConverter {
-    private static final String IPS;
+    private static String IPS;
 
     static {
-        IPS = NetUtils.getRealIpAddress().stream()
-                .reduce((s, s2) -> s + ", " + s2).orElse("");
+//        IPS = NetUtils.getRealIpAddress().stream()
+//                .reduce((s, s2) -> s + ", " + s2).orElse("");
+        try {
+            IPS = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            IPS = "NULL";
+            e.printStackTrace();
+        }
+
     }
 
     @Override
