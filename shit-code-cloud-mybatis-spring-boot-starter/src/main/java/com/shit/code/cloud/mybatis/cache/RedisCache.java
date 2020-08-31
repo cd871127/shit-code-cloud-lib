19a397a4a3e2932616191a68cca2e9952182033f
anthonychen <anthonychen@lexin.com>
2020-08-31 10:29:02 +0800

    fix

--- shit-code-cloud-mybatis-spring-boot-starter/src/main/java/com/shit/code/cloud/mybatis/cache/RedisCache.java
+++ shit-code-cloud-mybatis-spring-boot-starter/src/main/java/com/shit/code/cloud/mybatis/cache/RedisCache.java
@@ -12,12 +12,22 @@ import java.time.Duration;
  * https://www.cnblogs.com/cxuanBlog/p/11333021.html
  */
 public class RedisCache implements Cache {
-    BoundHashOperations<String, String, Object> boundHashOperations;
+    private BoundHashOperations<String, String, Object> boundHashOperations = null;
     private final String id;
 
-    @SuppressWarnings("unchecked")
     public RedisCache(String id) {
         this.id = id;
+    }
+
+    private BoundHashOperations<String, String, Object> getBoundHashOperations() {
+        if (boundHashOperations == null) {
+            bindKey();
+        }
+        return boundHashOperations;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void bindKey() {
         RedisTemplate<String, Object> redisTemplate =
                 (RedisTemplate<String, Object>) SpringContextUtil.getContext().getBean("redisTemplate", RedisTemplate.class);
         this.boundHashOperations = redisTemplate.boundHashOps(id);
@@ -30,29 +40,29 @@ public class RedisCache implements Cache {
 
     @Override
     public void putObject(Object key, Object value) {
-        boundHashOperations.put(key.toString(), value);
+        getBoundHashOperations().put(key.toString(), value);
     }
 
     @Override
     public Object getObject(Object key) {
-        return boundHashOperations.get(key.toString());
+        return getBoundHashOperations().get(key.toString());
     }
 
     @Override
     public Object removeObject(Object key) {
         Object object = getObject(key);
-        boundHashOperations.delete(key.toString());
+        getBoundHashOperations().delete(key.toString());
         return object;
     }
 
     @Override
     public void clear() {
-        boundHashOperations.expire(Duration.ZERO);
+        getBoundHashOperations().expire(Duration.ZERO);
     }
 
     @Override
     public int getSize() {
-        Long size = boundHashOperations.size();
+        Long size = getBoundHashOperations().size();
         return size == null ? 0 : Math.toIntExact(size);
     }
 }
--- shit-code-cloud-spring-boot-starter/src/main/java/shit/code/cloud/spring/config/SpringBootAutoConfiguration.java
+++ shit-code-cloud-spring-boot-starter/src/main/java/shit/code/cloud/spring/config/SpringBootAutoConfiguration.java
@@ -3,6 +3,8 @@ package shit.code.cloud.spring.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import shit.code.cloud.spring.utils.SpringContextUtil;
 
 @Configuration
